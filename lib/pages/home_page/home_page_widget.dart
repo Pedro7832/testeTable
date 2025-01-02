8f388278bedeff3cd5e08d74b7da997ffae0ddf8
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-02 20:21:12 +0000

    Updating to latest FlutterFlow output. 09

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -78,7 +78,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
                   width: 100.0,
                   height: 100.0,
                   decoration: BoxDecoration(
-                    color: FlutterFlowTheme.of(context).tertiary,
+                    color: FlutterFlowTheme.of(context).secondary,
                   ),
                 ),
               ],
