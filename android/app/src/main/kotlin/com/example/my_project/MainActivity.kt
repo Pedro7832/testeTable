898e7eace7231cf42ffe8061af320a1766ec14e0
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2025-01-02 19:19:50 +0000

    01

---
+++ android/app/src/main/kotlin/com/example/my_project/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.mycompany.tablesmart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/custom_code/widgets/table_smart.dart
@@ -0,0 +1,30 @@
+// Automatic FlutterFlow imports
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import 'index.dart'; // Imports other custom widgets
+import 'package:flutter/material.dart';
+// Begin custom widget code
+// DO NOT REMOVE OR MODIFY THE CODE ABOVE!
+
+class TableSmart extends StatefulWidget {
+  const TableSmart({
+    super.key,
+    this.width,
+    this.height,
+    this.dataJson,
+  });
+
+  final double? width;
+  final double? height;
+  final dynamic dataJson;
+
+  @override
+  State<TableSmart> createState() => _TableSmartState();
+}
+
+class _TableSmartState extends State<TableSmart> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
